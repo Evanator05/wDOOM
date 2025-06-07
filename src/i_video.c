f6a2cbeb6e47f585236b3735a468a227e0977b81
Evanator05 <watsonevan2005@gmail.com>
2025-06-06 20:54:29 -0600

    menu boots and is usable

--- src/i_video.c
+++ src/i_video.c
@@ -59,7 +59,7 @@ rcsid[] = "$Id: i_x.c,v 1.6 1997/02/03 22:45:10 b1 Exp $";
 
 #include "SDL3/SDL.h"
 
-#define DISPLAYSCALE 5
+int SDLDoomKey(SDL_Keycode keycode);
 
 SDL_Window *window;
 SDL_Renderer *renderer;
@@ -70,11 +70,10 @@ SDL_Color palette[256];
 byte* mainScreen;
 
 void I_InitGraphics(void) {
-	
 	if (!SDL_Init(SDL_INIT_VIDEO))
 		I_Error("SDL_Init failed: %s", SDL_GetError());
 	
-	window = SDL_CreateWindow("wDOOM", SCREENWIDTH*DISPLAYSCALE, SCREENHEIGHT*DISPLAYSCALE, 0);
+	window = SDL_CreateWindow("wDOOM", SCREENWIDTH, SCREENHEIGHT, SDL_WINDOW_FULLSCREEN);
 
 	if (!window)
 		I_Error("Window creation failed: %s", SDL_GetError());
@@ -96,9 +95,9 @@ void I_ShutdownGraphics(void) {
 
 void I_SetPalette (byte* doomPalette) {
 	for (int i = 0; i < 256; i++) {
-		palette[i].r = gammatable[usegamma][doomPalette[i*3+0]];
-		palette[i].g = gammatable[usegamma][doomPalette[i*3+1]];
-		palette[i].b = gammatable[usegamma][doomPalette[i*3+2]];
+		palette[i].r = gammatable[usegamma][*doomPalette++];
+		palette[i].g = gammatable[usegamma][*doomPalette++];
+		palette[i].b = gammatable[usegamma][*doomPalette++];
 		palette[i].a = 255;
 	}
 }
@@ -106,58 +105,120 @@ void I_SetPalette (byte* doomPalette) {
 void I_UpdateNoBlit (void) {
 	//printf("Update No Blit\n");
 }
+
 void I_FinishUpdate (void) {
 	void* pixels;
 	int pitch;
 	SDL_LockTexture(texture, NULL, &pixels, &pitch);
 
-	uint32_t *dest = (uint32_t *)pixels;
+	uint32_t *dest = (uint32_t*)pixels;
 
 	for (int y = 0; y < SCREENHEIGHT; y++) {
 		for (int x = 0; x < SCREENWIDTH; x++) {
-			//screens[0][y*SCREENWIDTH + x] = 0x1F;  // some palette index ≠ 0
-
 			byte colorIndex = mainScreen[y*SCREENWIDTH+x];
-
 			SDL_Color c = palette[colorIndex];
-
-			//printf("%d %d %d\n", c.r, c.g, c.b);
 
 			dest[y*(pitch/4)+x] = (c.r<<24) | (c.g<<16) | (c.b<<8) | 0xFF;
 		}
 	}
 
 	SDL_UnlockTexture(texture);
-
 	SDL_RenderTexture(renderer, texture, NULL, NULL);
 	SDL_RenderPresent(renderer);
 }
 
 void I_WaitVBL(int count) {
-	printf("hello %d", count);
 	SDL_Delay(1000*count/35);
 }
 
-void I_ReadScreen (byte* scr) {
+void I_ReadScreen(byte *scr) {
 	//printf("Read Screen\n");
 }
 
-void I_BeginRead (void) {
+void I_BeginRead(void) {
 	//printf("Begin Read\n");
 }
 
-void I_EndRead (void) {
+void I_EndRead(void) {
 	//printf("End Read\n");
 }
 
-void I_StartFrame (void) {
+void I_StartFrame(void) {
 	//printf("Start Frame\n");
 	SDL_Event e;
+	event_t d;
+	boolean did = false;
+	
 	while (SDL_PollEvent(&e)) {
-		if (e.type == SDL_EVENT_QUIT) {
-			I_Quit();
+		switch(e.type) {
+			case SDL_EVENT_QUIT:
+				I_Quit();
+				break;
+			case SDL_EVENT_KEY_DOWN:
+				d.type = ev_keydown;
+				d.data1 = SDLDoomKey(SDL_GetKeyFromScancode(e.key.scancode, SDL_KMOD_NONE, false));
+				did = d.data1 > 0;
+				break;
+			case SDL_EVENT_KEY_UP:
+				d.type = ev_keyup;
+				d.data1 = SDLDoomKey(SDL_GetKeyFromScancode(e.key.scancode, SDL_KMOD_NONE, false));
+				did = d.data1 > 0;
+				break;
+			case SDL_EVENT_MOUSE_MOTION: // needs better mouse implementation
+				// d.type = ev_mouse;
+				// d.data1 = 0;
+				// d.data2 = e.motion.xrel;
+				// d.data3 = -e.motion.yrel;
+				// did = true;
+				break;
+		}
+		
+		if (did) {
+			D_PostEvent(&d);
+			did = false;
 		}
 	}
+}
+
+int SDLDoomKey(SDL_Keycode keycode) {
+	int dcode;
+	switch (keycode) {
+		case SDLK_RIGHT: dcode = KEY_RIGHTARROW; break;
+		case SDLK_LEFT: dcode = KEY_LEFTARROW; break;
+		case SDLK_UP: dcode = KEY_UPARROW; break;
+		case SDLK_DOWN: dcode = KEY_DOWNARROW; break;
+
+		case SDLK_ESCAPE: dcode = KEY_ESCAPE; break;
+		case SDLK_RETURN: dcode = KEY_ENTER; break;
+		case SDLK_TAB: dcode = KEY_TAB; break;
+
+		case SDLK_F1: dcode = KEY_F1; break;
+		case SDLK_F2: dcode = KEY_F2; break;
+		case SDLK_F3: dcode = KEY_F3; break;
+		case SDLK_F4: dcode = KEY_F4; break;
+		case SDLK_F5: dcode = KEY_F5; break;
+		case SDLK_F6: dcode = KEY_F6; break;
+		case SDLK_F7: dcode = KEY_F7; break;
+		case SDLK_F8: dcode = KEY_F8; break;
+		case SDLK_F9: dcode = KEY_F9; break;
+		case SDLK_F10: dcode = KEY_F10; break;
+		case SDLK_F11: dcode = KEY_F11; break;
+		case SDLK_F12: dcode = KEY_F12; break;
+
+		case SDLK_BACKSPACE: dcode = KEY_BACKSPACE; break;
+		case SDLK_PAUSE: dcode = KEY_PAUSE; break;
+
+		case SDLK_EQUALS: dcode = KEY_EQUALS; break;
+		case SDLK_MINUS: dcode = KEY_MINUS; break;
+
+		case SDLK_RSHIFT: dcode = KEY_RSHIFT; break;
+		case SDLK_RCTRL: dcode = KEY_RCTRL; break;
+		case SDLK_RALT: dcode = KEY_RALT; break;
+		case SDLK_LALT: dcode = KEY_LALT; break;
+		
+		default: dcode = 0; break;
+	}
+	return dcode;
 }
 
 void I_StartTic(void) {
